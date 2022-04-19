7fe525ab47bf75a9313186a8d84b6c402ea53a82
Manjunath Kallole <manjunath.kallole@CDSYS.LOCAL>
2022-04-19 00:00:12 -0700

    Java demo 1 updated

---
+++ src/com/crest/demo/CountChar.java
@@ -0,0 +1,39 @@
+package com.crest.demo;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CountChar {
+
+	public static void main(String[] args) {
+		 Map<Character, Integer>  mp = countChar("Manjunath");
+		 for (Map.Entry<Character, Integer> mp2 : mp.entrySet() ) {
+			 System.out.println(mp2.getKey() +"--"+mp2.getValue());
+			
+		}
+		
+	}
+
+	private static Map<Character, Integer> countChar(String str) {
+		
+		
+		Map<Character, Integer> cMap = new HashMap<>();
+		
+		for(int i =0;i<str.length();i++)
+		{
+			if(cMap.containsKey(str.charAt(i)))
+			{
+				int j =cMap.get(str.charAt(i));
+				cMap.put(str.charAt(i), ++j);
+			}
+			else
+			{
+				cMap.put(str.charAt(i), 1);
+			}
+		}
+		return cMap;
+		
+		
+	}
+}
---
+++ src/com/crest/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.crest.demo;
+
+public class Demo1 {
+public static void main(String[] args) {
+	System.out.println("hello");
+	
+	String inputString ="9590533043";
+	String str =String.format("(%s)%s-%s-%s",inputString.substring(0,3),inputString.substring(3,4),inputString.substring(4,7),inputString.substring(7));
+System.out.println(str);
+}
+}
